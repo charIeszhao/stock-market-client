55c76fc65e81593b047ad3d972cdb13afeb12d14
demonzoo <demonzoo712027@gmail.com>
2020-04-27 21:56:48 +0800

    add dashboard page and echart library

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+export type MarketData = {
+  companyName: string;
+  lastPrice: number;
+  priorDatePrice: number;
+};
+
+export type SectorData = {
+  sectorName: string;
+  changePct: number;
+};
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,9 +18,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  options: any;
+  marketDataEntries: MarketData[];
+  sectorDataEntries: SectorData[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.marketDataEntries = [
+      { companyName: 'Company 1', lastPrice: 20.04, priorDatePrice: 20.11 },
+      { companyName: 'Company 2', lastPrice: 1.63, priorDatePrice: 1.65 },
+      { companyName: 'Company 3', lastPrice: 0.74, priorDatePrice: 0.72 },
+      { companyName: 'Company 4', lastPrice: 0.95, priorDatePrice: 0.94 },
+      { companyName: 'Company 5', lastPrice: 0.92, priorDatePrice: 0.94 }
+    ];
+
+    this.sectorDataEntries = [
+      { sectorName: 'Energy', changePct: -0.46 },
+      { sectorName: 'Financial', changePct: 0.33 },
+      { sectorName: 'Health Care', changePct: 3.31 },
+      { sectorName: 'Industrial', changePct: 1.89 },
+      { sectorName: 'Information Technology', changePct: 4.17 }
+    ];
+
+    const xAxisData = [];
+    const data1 = [];
+    const data2 = [];
+
+    for (let i = 0; i < 100; i++) {
+      xAxisData.push('category' + i);
+      data1.push((Math.sin(i / 5) * (i / 5 - 10) + i / 6) * 5);
+      data2.push((Math.cos(i / 5) * (i / 5 - 10) + i / 6) * 5);
+    }
+
+    this.options = {
+      legend: {
+        data: ['Company 1', 'Company 2'],
+        align: 'left'
+      },
+      tooltip: {},
+      xAxis: {
+        data: xAxisData,
+        silent: false,
+        splitLine: {
+          show: false
+        }
+      },
+      yAxis: {
+      },
+      series: [{
+        name: 'bar',
+        type: 'bar',
+        data: data1,
+        animationDelay(idx) {
+          return idx * 10;
+        }
+      }, {
+        name: 'bar2',
+        type: 'bar',
+        data: data2,
+        animationDelay(idx) {
+          return idx * 10 + 100;
+        }
+      }],
+      animationEasing: 'elasticOut',
+      animationDelayUpdate(idx) {
+        return idx * 5;
+      }
+    };
   }
 
 }
---
+++ src/app/pipes/stock-price-change.pipe.ts
@@ -0,0 +1,26 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
+@Pipe({
+  name: 'stockPriceChange'
+})
+export class StockPriceChangePipe implements PipeTransform {
+
+  constructor(
+    private sanitizer: DomSanitizer
+  ) {
+  }
+
+  transform(lastValue: number, priorDatePrice: number): SafeHtml {
+    const change: number = Number((lastValue - priorDatePrice).toFixed(2));
+    const color: string = change > 0 ? '#00CC00' : 'red';
+    const changePct = this.forceShowPlusSign(Math.round(10000 * change / priorDatePrice) / 100);
+    const changeStr = this.forceShowPlusSign(change);
+    const html = `<span style="color: ${color}">${lastValue} (${changeStr} , ${changePct}%)</span>`;
+    return this.sanitizer.bypassSecurityTrustHtml(html);
+  }
+
+  forceShowPlusSign(value: number): string {
+    return (value > 0 ? '+' : '') + value;
+  }
+}
